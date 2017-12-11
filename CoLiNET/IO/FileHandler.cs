1ee8bd4e9469a77d6749c0007e1e1c5568bb47f2
Ben Wolf <benjamin.wolf@innoq.com>
2017-12-12 00:02:04 +0100

    Add first empty class

---
+++ CoLiNET/IO/FileHandler.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Ben Wolf (programming-wolf) and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+namespace De.ProgrammingWolf.Common.IO
+{
+    /// <summary>
+    /// Class provides several methods to deal with files easily.
+    /// </summary>
+    public static class FileHandler
+    {
+    }
+}
